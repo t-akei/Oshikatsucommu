010f9b32a9de07b5e7f472f803d8deceda63ca7e
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-10 11:24:25 +0000

    [投稿機能作成]

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -4,10 +4,10 @@ class Public::PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new
-    @post.user_id = current_user.id
-    @post.save
-    redirect_to post_path(@post)
+    post = Post.new(post_params)
+    post.user_id = current_user.id
+    post.save
+    redirect_to post_path(@post.id)
   end
 
   def show
@@ -20,17 +20,21 @@ class Public::PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
   end
 
   def destroy
   end
-  
-  
+
+
   private
-  
+
   def post_params
     params.require(:post).permit(:title, :body)
     #:genre_id,を追加すること
