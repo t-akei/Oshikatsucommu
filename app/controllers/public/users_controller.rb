e55db280bfcc8d5c7f7ba34478fbcccc2a527d0e
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-12 07:34:25 +0000

    [部分テンプレート]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,8 +2,8 @@ class Public::UsersController < ApplicationController
   # before_action :configure_permitted_parameters, if: :devise_controller?
 
   def show
-    user = User.find(params[:id])
-    @posts = Post.where(user_id: user.id)
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id)
     # @user = User.find(params[:id])
   end
 
