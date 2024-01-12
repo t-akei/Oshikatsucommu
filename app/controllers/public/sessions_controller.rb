2f63cd7c1065c200291c77351956652e7bcca547
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-12 11:21:51 +0000

    [Add flash]

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -17,11 +17,14 @@ class Public::SessionsController < Devise::SessionsController
   # def destroy
   #   super
   # end
-  
+
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
 
+  def after_sign_out_path_for(resource)
+    root_path
+  end
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
