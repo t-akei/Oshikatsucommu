98100904f4ecc4f225233787aaf3e18f482db4fe
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-13 08:19:54 +0000

    [Add search機能]

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,2 +1,6 @@
 class Public::SearchesController < ApplicationController
+
+  def search
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,20 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
-  
+
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-  
+
   root to: 'public/homes#top'
-  
+  get 'search' => 'public/searches#search'
+
   scope module: :public do
     resources :posts
     resources :users, only: [:show, :edit, :update]
   end
-  
+
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
