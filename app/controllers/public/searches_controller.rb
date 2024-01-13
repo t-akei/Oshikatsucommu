451395a73939298362e3ffbb3cc857e1c74777b9
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-13 08:56:37 +0000

    [search機能]

--- app/controllers/public/searches_controller.rb
+++ app/controllers/public/searches_controller.rb
@@ -1,6 +1,11 @@
 class Public::SearchesController < ApplicationController
+  before_action :authenticate_user!
 
   def search
+    @model = params[:post]
+    @content = params[:content]
+    @method = params[:method]
+    @results = Post.search_for(@content, @method)
   end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,9 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 250 }
 
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Post.where(title: content, body: content)
+    elsif method == 'forward'
+      Post.where
 end
