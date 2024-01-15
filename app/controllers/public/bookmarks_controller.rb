9fe37804bf64aba163991000232c088a32d100d5
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-15 08:00:29 +0000

    [Booknark機能]

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -1,7 +1,25 @@
 class Public::BookmarksController < ApplicationController
+  before_action :authenticate_user!
+
   def create
+    @post = Post.find(params[:id])
+    bookmark = @post.bookmarks.new(user_id: current_user.id)
+    if bookmark.save
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    bookmark = @post.bookmarks.find_by(user_id: current_user.id)
+    if bookmark.present?
+      bookmark.destroy
+      redirect_to request.referer
+    else
+      redirect_to request.referer
+    end
   end
+
 end
