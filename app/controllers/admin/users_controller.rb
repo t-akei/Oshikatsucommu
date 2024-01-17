28576535089b71cc03e239e14174a1063fa5502d
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-17 15:32:07 +0000

    [管理者機能]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,8 +1,11 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_admin!
 
+  def show
+  end
+  
   def index
     @users = User.all
   end
-# attribute
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,7 +28,7 @@ Rails.application.routes.draw do
 
   namespace :admin do
     root to: 'users#index'
-    resources :users, only: [:index]
+    resources :users, only: [:show, :index]
   end
 
 end
