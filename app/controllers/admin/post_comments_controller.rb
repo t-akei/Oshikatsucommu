ebd647ac21ad2cd75face8af5430ee6ee7edd830
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-18 10:37:25 +0000

    [管理者コメント削除機能]

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -1,6 +1,7 @@
 class Admin::PostCommentsController < ApplicationController
   before_action :authenticate_admin!
-  
+
   def index
+    @post_comments = PostComment.all.order(created_at: :desc)
   end
 end
