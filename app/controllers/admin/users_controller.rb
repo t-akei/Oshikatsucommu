941339e7de3bd60ba5abc12bceec05622caca524
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-21 17:54:15 +0000

    [admin Add_post_show]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,6 +3,7 @@ class Admin::UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @posts = Post.where(user_id: @user.id).order(created_at: :desc)
   end
 
   def index
