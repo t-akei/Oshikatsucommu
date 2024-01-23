e835ed6f2901a9c66000f42d06861159cd76ab9b
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-23 19:15:30 +0000

    [レイアウト&機能微調整]

--- app/controllers/public/genre_searches_controller.rb
+++ app/controllers/public/genre_searches_controller.rb
@@ -3,7 +3,7 @@ class Public::GenreSearchesController < ApplicationController
 
   def search
     @genre = Genre.find(params[:genre_id])
-    @posts = Post.where(genre_id: @genre.id)
+    @posts = Post.where(genre_id: @genre.id).order(created_at: :desc)
     # 投稿の内、genre_idに指定されたジャンルのIDを持つ投稿を全て取得
     @user = current_user
     @genres = Genre.all
