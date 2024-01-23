5bf97e7b32d88ad18f5267d2b6b6ba77331f2803
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-23 09:47:07 +0000

    [レイアウト&機能微調整]

--- app/controllers/public/tagsearches_controller.rb
+++ app/controllers/public/tagsearches_controller.rb
@@ -3,7 +3,7 @@ class Public::TagsearchesController < ApplicationController
 
   def search
     @content = params[:content]
-    @posts = Post.where('category LIKE ?', '%' + @content + '%')
+    @posts = Post.where('category LIKE ?', '%' + @content + '%').order(created_at: :desc)
     @user = current_user
     @genres = Genre.all
   end
