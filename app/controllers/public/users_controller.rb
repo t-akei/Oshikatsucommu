3d7a6e9fafaa623e034a961481984b905922b182
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-24 12:10:28 +0000

    [レイアウト&機能微調整]

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -43,7 +43,7 @@ class Public::UsersController < ApplicationController
   def ensure_guest_user
     @user = User.find(params[:id])
     if @user.email == "guest@example.com"
-      flash[:notice] = "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+      flash[:alert] = "ゲストユーザーはプロフィール編集画面へ遷移できません。"
       redirect_to user_path(current_user)
     end
   end
