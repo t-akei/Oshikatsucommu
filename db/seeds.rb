d43eedc6660ee1908263a0d774fcbac15aec271a
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-27 07:32:24 +0000

    [Adminログイン]

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Admin.create!(email: "oshik@gmail.com",
-              password: "pfoshi")
+Admin.create!(email: ENV['ADMIN_EMAIL'],
+               password: ENV['ADMIN_PASS'])
