39d42be78fe7db7f3511f9f3537bd6f8662c930f
t-akei <145541870+t-akei@users.noreply.github.com>
2024-01-28 13:56:45 +0000

    [レイアウト調整]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -13,7 +13,7 @@ class Admin::GenresController < ApplicationController
 
   def index
     @genre = Genre.new
-    @genres = Genre.all
+    @genres = Genre.all.order(created_at: :desc).page(params[:page]).per(7)
   end
 
   def edit
@@ -34,5 +34,6 @@ class Admin::GenresController < ApplicationController
   def genre_params
     params.require(:genre).permit(:name)
   end
+
 
 end
