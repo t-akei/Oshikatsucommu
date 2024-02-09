44e1b751ab64ff6b5732b96093cd37a3f73f5c7b
t-akei <145541870+t-akei@users.noreply.github.com>
2024-02-09 18:03:41 +0000

    [post_controller修正]

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -7,7 +7,8 @@ class Admin::GenresController < ApplicationController
     if @genre.save
       redirect_to admin_genres_path
     else
-      render :index
+      redirect_to admin_genres_path
+      # render :index
     end
   end
 
